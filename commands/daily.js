f65787abc6b58f15532cf9fd4ce13c40ce630c81
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-07-29 03:03:11 -0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -7,7 +7,7 @@ module.exports = {
         const embedMsg = new EmbedBuilder();
 
         const zeroPad = (num, places) => String(num).padStart(places, '0');
-        var pokedexEntry = zeroPad(getRandomInt(905) + 1, 3);
+        var pokedexEntry = zeroPad(Math.floor(Math.random() * 905) + 1, 3);
         var image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokedexEntry}.png`;
         embedMsg.setColor('FFF000');
         embedMsg.setTitle('Pokemon of the Day!');
